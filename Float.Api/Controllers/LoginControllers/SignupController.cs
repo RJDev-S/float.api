6788eb4b9fb693c9a7df996f016253e1b60a8cba
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-12-16 15:17:51 +0800

    added first controllers and models

---
+++ Float.Api/Controllers/LoginControllers/SignupController.cs
@@ -0,0 +1,28 @@
+﻿using Float.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Float.Api.Controllers.LoginControllers
+{
+    [ApiController]
+
+    public class SignupController : Controller
+    {
+        SignupModel _signupModel = new SignupModel();
+
+
+        public SignupController()
+        {
+
+        }
+
+        [HttpGet("api/v1/get/signup")]
+        public IActionResult GetSignup()
+        {
+            return Ok(_signupModel);
+        }
+    }
+}
---
+++ Float.Core/Model/SignupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Core.Model
+{
+   public class SignupModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; } = "Rj";
+        public string Password { get; set; } = "1234";
+        public string DateCreated { get; set; } = DateTime.Now.ToString();
+        public string Type { get; set; } = "Local";
+    }
+}
