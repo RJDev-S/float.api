727d20e98f383733b42ecec7fe8519d323b3cfe4
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-12-21 15:36:26 +0800

    - - added core folder and application and domain library

---
+++ Float.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Domain.Common
+{
+   public abstract class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedby { get; set; }
+        public DateTime LastModified { get; set; }
+
+    }
+}
---
+++ Float.Domain/Entities/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿using Float.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Domain.Entities
+{
+    public class UserAccount : AuditableBaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string LoginType { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
