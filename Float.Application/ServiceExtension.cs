26104473c601c4c671eb6a30b7a16f069429ec57
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-01-09 21:26:23 +0800

    - - edited account service

---
+++ Float.Application/ServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Float.Application.Interfaces;
+using Float.Application.Services.AccountServices;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Float.Application
+{
+    public static class ServiceExtension
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(ISignupService), typeof(SignupService));
+        }
+    }
+}
---
+++ Float.Application/Services/AccountServices/SignupService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Float.Application.DTOs.Account;
+using Float.Application.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Float.Application.Services.AccountServices
+{
+    public class SignupService : ISignupService
+    {
+        private readonly IMapper _mapper;
+
+        public SignupService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        //public async Task<ServiceResponse<Signup>> AddUserinDb(SignupDto newUser)
+        //{
+        //    ServiceResponse<Signup> serviceResponseModel = new ServiceResponse<Signup>();
+        //    try
+        //    {
+        //     ;
+        //        serviceResponseModel.Data = _mapper.Map<Signup>(newUser);
+        //        serviceResponseModel.IsSuccess = true;
+        //        serviceResponseModel.Message = "Sign up successful!";
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        serviceResponseModel.Data = null;
+        //        serviceResponseModel.IsSuccess = false;
+        //        serviceResponseModel.Message = e.Message;
+        //    }
+
+        //    return serviceResponseModel;
+        //}
+
+        public async Task<string> AddUserinDb(UserAccountDTO newUser)
+        {
+            return "Go to this url to verify your account";
+        }
+    }
+}
