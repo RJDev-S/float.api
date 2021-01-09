d6755b146f74873c26bd3b8e746c50ca7637cb40
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-01-09 21:25:22 +0800

    - - added DTO

---
+++ Float.Application/DTOs/Account/UserAccountDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Float.Application.DTOs.Account
+{
+   public class UserAccountDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
