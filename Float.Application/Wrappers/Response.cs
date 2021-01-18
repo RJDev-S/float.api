e5b9955dc0ce19ae186ef8feac444d09b3050b0b
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-01-18 14:38:18 +0800

    - - added response wrapper

---
+++ Float.Application/Wrappers/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Application.Wrappers
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+
+        }
+
+        public Response(T data, string message = null)
+        {
+            IsSuccess = true;
+            Data = data;
+            Message = message;
+        }
+
+        public Response(string message)
+        {
+            IsSuccess = false;
+            Message = message;
+        }
+
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public bool IsSuccess { get; set; }
+
+    }
+}
