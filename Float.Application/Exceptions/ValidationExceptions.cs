af40256691cd8ec7a41916e2095e8c1a7392dd37
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-01-21 21:56:33 +0800

    added validation exceptions

---
+++ Float.Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Application.Exceptions
+{
+   public class ValidationExceptions : Exception
+    {
+        public ValidationExceptions() : base("One or more validation failures occured")
+        {
+            Errors = new List<string>();
+        }
+
+        public List<string> Errors { get; }
+
+        public ValidationExceptions(IEnumerable<string> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure);
+            }    
+        }
+
+    }
+}
--- Float.Application/Wrappers/Response.cs
+++ Float.Application/Wrappers/Response.cs
@@ -13,20 +13,21 @@ namespace Float.Application.Wrappers
 
         public Response(T data, string message = null)
         {
-            IsSuccess = true;
             Data = data;
             Message = message;
+            Succeeded = true;
+
         }
 
-        public Response(string message)
+        public Response(string message, bool succeeded = false)
         {
-            IsSuccess = false;
+            Succeeded = succeeded;
             Message = message;
         }
 
         public T Data { get; set; }
-        public bool IsSuccess { get; set; }
         public string Message { get; set; }
+        public bool Succeeded { get; set; }
         public List<string> Errors { get; set; }
 
     }
