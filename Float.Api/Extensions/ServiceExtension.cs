6a2fb9c144400dea14a78ecb902dfc70c6e3c0fd
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-02-02 21:39:51 +0800

    added service extenstion for cleaner and easy to read code

---
+++ Float.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Float.Api.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void AddSwaggerExtension(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Float.Api", Version = "v1" });
+            });
+        }
+    }
+}
