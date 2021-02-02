e6ae44cc19c9c0b64eb062c49129667a9f344a2f
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-02-02 21:40:21 +0800

    registers extension classes

--- Float.Api/Startup.cs
+++ Float.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Float.Api.Extensions;
 using Float.Api.Middleware;
 using Float.Application;
 using Float.Infrastracture.Identity;
@@ -30,11 +31,8 @@ namespace Float.Api
             services.AddApplicationLayer();
             services.AddIdentityInfrastracture(_config);
             services.AddPersistenceInfrastracture(_config);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Float.Api", Version = "v1" });
-            });
+            services.AddSwaggerExtension();
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,15 +41,16 @@ namespace Float.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Float.Api v1"));
+              
             }
             //app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseSerilogRequestLogging();
             app.UseAuthorization();
-            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseSwaggerExtension();
+            app.UseExceptionMiddleWare();
+            app.UseHealthChecks("/health");
 
             app.UseEndpoints(endpoints =>
             {
