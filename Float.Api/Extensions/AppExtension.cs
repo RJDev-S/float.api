defa8c21a3de9122dcc65c1b37f96d8548ccb839
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-02-02 21:39:30 +0800

    added appextension for cleaner and code reusablity

---
+++ Float.Api/Extensions/AppExtension.cs
@@ -0,0 +1,18 @@
+﻿using Float.Api.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Float.Api.Extensions
+{
+    public static class AppExtension
+    {
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Float.Api v1"));
+        }
+        public static void UseExceptionMiddleWare(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+    }
+}
