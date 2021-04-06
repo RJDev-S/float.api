72e615666c94f0b518e97e3e3abc43d7a6b0f746
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-04-06 16:38:16 +0800

    added RegisterDTO

---
+++ Float.Application/DTOs/Account/RegisterResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Float.Application.DTOs.Account
+{
+   public class RegisterResponse
+    {
+        public string Username { get; set; }
+    }
+}
